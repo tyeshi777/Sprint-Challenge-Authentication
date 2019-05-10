d31348c2eb0f7c7c78f64ef55d503884b793bb82
Yeshi <tyeshi777@yahoo.com>
2019-05-10 08:50:53 -0700

    register down

---
+++ auth/token-service.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets.js");
+
+module.exports = {
+  generateToken
+};
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    roles: ["user"]
+  };
+  const options = {
+    expiresIn: "1d"
+  };
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,9 @@
 const axios = require("axios");
+const express = require("express");
 const bcrypt = require("bcryptjs");
+const server = express();
+
+const Users = require("./routes-model.js");
 
 const { authenticate } = require("../auth/authenticate");
 
@@ -11,10 +15,41 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  server.post("http://localhost:3300/api/register");
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function login(req, res) {
   // implement user login
+  server.post("http://localhost:3300/api/login");
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = tokenService.generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!, have a token...`,
+          token,
+          roles: token.roles
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function getJokes(req, res) {
